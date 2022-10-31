c099f79587164203111103ad4a87eba3f480d3be
CerineExists <c.c.touber@students.uu.nl>
2022-10-31 21:46:06 +0100

    animatie af
    De raket geeft vlammen als de w ingedrukt is :)

--- src/View.hs
+++ src/View.hs
@@ -7,18 +7,25 @@ import Model
 
 -- | Tekenen
 viewBMP :: World -> IO Picture
-viewBMP (World (Player (Location x y) degree v) keys as bullets state score (PicList r [r1, r2] s a) _ time _) 
+viewBMP (World (Player (Location x y) degree v) keys as bs state score (PicList r [r1, r2, r3, r4] space a) _ time _) 
                             | state == Playing = return $ pictures scene
                             | otherwise = return $ pictures ( scene ++   [translate (-180) (-35) $ color white (text "Pause")]  )      
                                 where
-                                    scene = [s,  
-                                            translate x y $ rotate (90 - angle degree) rocket] ++
-                                            map translateBullets bullets ++
-                                            map (translateAsteroid a) as 
-                                            ++ scoreText score ++ timeText time
-                                    rocket  | even (round (time*24)) =  r1
-                                            | otherwise = r2
-
+                                        scene = [space,  
+                                                rocket] ++
+                                                bullets ++
+                                                asteroids ++
+                                                scoreText score ++ timeText time
+                                        rocket  = translate x y $ rotate (90 - angle degree) whichRocket
+                                        bullets = map translateBullets bs
+                                        asteroids = map (translateAsteroid a) as 
+                                        whichRocket     | 'w' `notElem` keys = r
+                                                        | rest == 0 =  r1
+                                                        | rest == 1 = r2
+                                                        | rest == 2 = r3
+                                                        | otherwise = r4 
+                                        rest = round (time*24) `mod` 4
+                                        
 -- op basis van elapsedTime één vd 2 sprites te kiezen                                                     
 
 translateAsteroid :: Picture -> Asteroid -> Picture 
